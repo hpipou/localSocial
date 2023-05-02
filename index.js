b10fb10b430c0295d806e36b36d68f18a956d115
hpipou <mr.aoudjit@gmail.com>
2023-05-02 23:42:52 +0200

    LIMIT EACH IP TO 100 REQUESTS PER 15 MINUTES

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.use(cors())
 const helmet = require("helmet");
 app.use(helmet());
 
+// limite le nombre de requêtes provenant d'une adresse IP 
+const rateLimit = require('express-rate-limit')
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(limiter)
+
 // token verification
 const tokenVerif = require("./middleware/tokenVerification")
 
